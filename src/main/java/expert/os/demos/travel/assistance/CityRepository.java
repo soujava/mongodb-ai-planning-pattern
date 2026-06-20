f003ddc916675086314aab285a65d5bcfd110bdc
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-20 18:20:25 +0100

    Use Jakarta Data BasicRepository and add findByCountry query to CityRepository

--- src/main/java/expert/os/demos/travel/assistance/CityRepository.java
+++ src/main/java/expert/os/demos/travel/assistance/CityRepository.java
@@ -1,10 +1,13 @@
 package expert.os.demos.travel.assistance;
 
-import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.BasicRepository;
 import jakarta.data.repository.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CityRepository extends CrudRepository<City, UUID> {
+public interface CityRepository extends BasicRepository<City, UUID> {
+
+    List<City> findByCountry(String country);
 }
