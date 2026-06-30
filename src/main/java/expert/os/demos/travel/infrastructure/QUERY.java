0fbc7a69a31c1ca1880dcf6e3eec8d45fabd072f
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-30 19:40:00 +0100

    Add custom JAX-RS @QUERY HTTP method annotation

---
+++ src/main/java/expert/os/demos/travel/infrastructure/QUERY.java
@@ -0,0 +1,16 @@
+package expert.os.demos.travel.infrastructure;
+
+import jakarta.ws.rs.HttpMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod("QUERY")
+public @interface QUERY {
+
+}
+
