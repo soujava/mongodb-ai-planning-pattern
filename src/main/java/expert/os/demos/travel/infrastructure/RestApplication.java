f1f67d167ae40b48cfa4edfbeb4c74c9fa2d242c
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-30 19:40:05 +0100

    Add JAX-RS RestApplication with /rest application path

---
+++ src/main/java/expert/os/demos/travel/infrastructure/RestApplication.java
@@ -0,0 +1,8 @@
+package expert.os.demos.travel.infrastructure;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestApplication extends Application {
+}
