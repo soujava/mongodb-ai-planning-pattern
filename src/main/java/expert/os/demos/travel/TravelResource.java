0b7e72ed757007b00376d3076aadfe263e5c2f02
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-01 07:12:51 +0100

    Add /travels path and QUERY annotation to TravelResource search method

--- src/main/java/expert/os/demos/travel/TravelResource.java
+++ src/main/java/expert/os/demos/travel/TravelResource.java
@@ -1,8 +1,10 @@
 package expert.os.demos.travel;
 
+import expert.os.demos.travel.infrastructure.QUERY;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -11,11 +13,13 @@ import java.util.List;
 @ApplicationScoped
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Path("/travels")
 public class TravelResource {
 
     @Inject
     private TravelService travelService;
 
+    @QUERY
     public List<Travel> search(TravelFilterRequest filter) {
         return travelService.search(filter);
     }
