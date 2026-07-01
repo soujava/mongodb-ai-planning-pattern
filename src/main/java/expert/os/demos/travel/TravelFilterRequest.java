9f8c7d08b0efcb0154212069f54f1a53805c97c0
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-01 07:34:55 +0100

    add travel entity record for nosql persistence

---
+++ src/main/java/expert/os/demos/travel/TravelFilterRequest.java
@@ -0,0 +1,42 @@
+package expert.os.demos.travel;
+
+import expert.os.demos.travel.infrastructure.FieldVisibilityStrategy;
+import jakarta.json.bind.annotation.JsonbVisibility;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@JsonbVisibility(value = FieldVisibilityStrategy.class)
+public class TravelFilterRequest {
+
+    private String city;
+    private TravelType type;
+    private BigDecimal minPrice;
+    private BigDecimal maxPrice;
+
+    public Optional<String> city() {
+        return Optional.ofNullable(city);
+    }
+
+    public Optional<TravelType> type() {
+        return Optional.ofNullable(type);
+    }
+
+    public Optional<BigDecimal> minPrice() {
+        return Optional.ofNullable(minPrice);
+    }
+
+    public Optional<BigDecimal> maxPrice() {
+        return Optional.ofNullable(maxPrice);
+    }
+
+    @Override
+    public String toString() {
+        return "TravelRequest{" +
+                "city='" + city + '\'' +
+                ", type=" + type +
+                ", minPrice=" + minPrice +
+                ", maxPrice=" + maxPrice +
+                '}';
+    }
+}
