f75676cd6df44fcf21e949757ce3c7fb79c759f0
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-01 07:27:59 +0100

    Remove unused import for Collections in TravelService

--- src/main/java/expert/os/demos/travel/TravelType.java
+++
@@ -1,6 +0,0 @@
-package expert.os.demos.travel;
-
-public enum TravelType {
-    BUSINESS,
-    LEISURE
-}
--- src/main/java/expert/os/demos/travel/infrastructure/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package expert.os.demos.travel.infrastructure;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class RestApplication extends Application {
-}
