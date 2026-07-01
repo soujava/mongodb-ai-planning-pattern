4f07d11c3c5bf60ddc6e128678b9fb8223540325
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-01 07:01:12 +0100

    Add TravelRequest DTO with city, type, and price range fields

---
+++ src/main/java/expert/os/demos/travel/TravelRequest.java
@@ -0,0 +1,13 @@
+package expert.os.demos.travel;
+
+import java.math.BigDecimal;
+
+
+public class TravelRequest {
+
+    private String city;
+    private TravelType type;
+    private BigDecimal minPrice;
+    private BigDecimal maxPrice;
+
+}
