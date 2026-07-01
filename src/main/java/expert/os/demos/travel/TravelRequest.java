7d13a48d5b061c43d7e5a6869d8699dbdcd13145
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-01 07:04:02 +0100

    Add JSON-B field visibility and Optional accessors to TravelRequest

--- src/main/java/expert/os/demos/travel/TravelRequest.java
+++ src/main/java/expert/os/demos/travel/TravelRequest.java
@@ -1,8 +1,12 @@
 package expert.os.demos.travel;
 
+import expert.os.demos.travel.infrastructure.FieldVisibilityStrategy;
+import jakarta.json.bind.annotation.JsonbVisibility;
+
 import java.math.BigDecimal;
+import java.util.Optional;
 
-
+@JsonbVisibility(value = FieldVisibilityStrategy.class)
 public class TravelRequest {
 
     private String city;
@@ -10,4 +14,19 @@ public class TravelRequest {
     private BigDecimal minPrice;
     private BigDecimal maxPrice;
 
+    public Optional<String> city() {
+        return Optional.ofNullable(city);
+    }
+
+    public Optional<TravelType> type() {
+        return Optional.ofNullable(type);
+    }
+
+    public Optional<BigDecimal> minPrice() {
+        return Optional.ofNullable(minPrice);
+    }
+
+    public Optional<BigDecimal> maxPrice() {
+        return Optional.ofNullable(maxPrice);
+    }
 }
