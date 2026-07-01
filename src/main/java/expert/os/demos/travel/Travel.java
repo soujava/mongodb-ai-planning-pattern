d671ceaf74e38abbcf71750c02422df60d78555a
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-01 06:47:58 +0100

    Add Travel JNoSQL entity with UUID id and City column

---
+++ src/main/java/expert/os/demos/travel/Travel.java
@@ -0,0 +1,17 @@
+package expert.os.demos.travel;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Travel {
+
+    @Id
+    private UUID id;
+
+    @Column
+    private City city;
+}
